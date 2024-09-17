8d92dfadaaa660a6399f775906f423300a343724
Matt Dennis <matthew.dennis@port.ac.uk>
2024-09-17 18:08:59 +0100

    works apart from sort

--- public/index.js
+++ public/index.js
@@ -1,8 +1,92 @@
 const data = await fetchData();
-
+const ui = {};
+ui.main = document.querySelector('main');
+ui.levels = document.querySelectorAll('section.level');
 
 async function fetchData() {
   const response = await fetch('./data/data.json');
   return await response.json();
 }
 
+function getLevelText(level) {
+  const texts = {
+    "L4": "Level 4",
+    "L5": "Level 5",
+    "L6": "Level 6",
+    "L7": "Level 7",
+  };
+
+  return texts[level];
+}
+
+
+
+function populate() {
+  for (const [level, plans] of Object.entries(data.plans)) {
+    //const levelObj = data[level];
+    const levelHolder = ui.main.querySelector(`section[data-level="${level}"]`);
+    const levelHead = document.createElement('h2');
+    levelHead.textContent = `${getLevelText(level)} courses`;
+    levelHolder.append(levelHead);
+    for (const plan of plans) {
+      const planSect = document.querySelector('#course-plan').content.cloneNode(true).firstElementChild;
+      levelHolder.append(planSect);
+      planSect.querySelector('h3').textContent = plan.title;
+      // handle common events for all courses first
+      if (data[level] && data[level].common) {
+        for (const event of data[level].common.events) {
+          if (event.not && event.not.includes(plan.code)) continue;
+          const day = planSect.querySelector(`article[data-day="${event.day}"]`);
+          populateEvent(day, event);
+        }
+      }
+
+      // handle specific course events.
+      if (data[level] && data[level][plan.code]) {
+        for (const event of data[level][plan.code].events) {
+          const day = planSect.querySelector(`article[data-day="${event.day}"]`);
+          populateEvent(day, event);
+        }
+      }
+
+      sortEvents(planSect);
+    }
+
+  }
+}
+
+function sortEvents(plan) {
+  const days = ["monday", "tuesday", "wednesday", "thursday", "friday"];
+  for (const day of days) {
+    const events = plan.querySelectorAll(`article[data-day="${day}"] div`);
+    for (let i = 0; i < events.length - 1; i++) {
+      if (events[i].dataset.startTime < events[i + 1].dataset.startTime) {
+        console.log('triggered');
+        events[i].parentElement.prepend(events[i]);
+      }
+    }
+  }
+}
+
+function populateEvent(day, event) {
+  for (const ph of day.querySelectorAll('.placeholder')) {
+    ph.remove();
+  }
+  const eventElem = document.querySelector('#event-template').cloneNode(true).content.firstElementChild;
+  day.append(eventElem);
+  const startTimeEl = eventElem.querySelector('.time');
+  const endTimeEl = eventElem.querySelector('.endtime');
+  let endTime = Number.parseInt(event.time) + 1;
+  if (event.duration) {
+    endTime = Number.parseInt(event.time) + Number.parseInt(event.duration);
+  }
+  startTimeEl.textContent = `${event.time}:00`;
+  endTimeEl.textContent = `${endTime}:00`;
+  eventElem.querySelector('.title').textContent = event.title;
+  eventElem.querySelector('.description').textContent = event.description;
+  eventElem.querySelector('.building').textContent = `${event.building} Building`;
+  eventElem.querySelector('.room').textContent = event.room;
+  eventElem.dataset.startTime = event.time;
+}
+
+populate();
