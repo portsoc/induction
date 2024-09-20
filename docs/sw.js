4a872ac7ad2be8c1ab38358828623b2449c8ab13
Matt Dennis <matthew.dennis@port.ac.uk>
2024-09-20 17:43:21 +0100

    nope.

--- docs/sw.js
+++ docs/sw.js
@@ -5,6 +5,7 @@ self.addEventListener('install', event => {
   event.waitUntil((async () => {
     const cache = await caches.open(CACHE_NAME);
     cache.addAll([
+      './',
       'index.html',
       'index.js',
       'style.css',
