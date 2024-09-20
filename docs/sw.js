e09a6296f93c834568993e6b80e6062d0e9bd46e
Matt Dennis <matthew.dennis@port.ac.uk>
2024-09-20 17:40:28 +0100

    will this fix it?

--- docs/sw.js
+++ docs/sw.js
@@ -1,11 +1,10 @@
-const CACHE_NAME = 'induction-tt-v1.4';
+const CACHE_NAME = 'induction-tt-v1.5';
 
 // Use the install event to pre-cache all initial resources.
 self.addEventListener('install', event => {
   event.waitUntil((async () => {
     const cache = await caches.open(CACHE_NAME);
     cache.addAll([
-      '/induction',
       'index.html',
       'index.js',
       'style.css',
