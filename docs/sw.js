c92d822bf8e8f31d3aba916e1f390eb796c4b96d
Matt Dennis <matthew.dennis@port.ac.uk>
2024-09-20 17:13:01 +0100

    PWA enabled

---
+++ docs/sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'induction-tt-v1.2';
+
+// Use the install event to pre-cache all initial resources.
+self.addEventListener('install', event => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    cache.addAll([
+      '/',
+      '/index.html',
+      '/index.js',
+      '/style.css',
+      '/data/data.json',
+      'logo.svg',
+    ]);
+  })());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    }),
+  );
+});
